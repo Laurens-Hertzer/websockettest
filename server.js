6d489538314cc112c026a36b6c17be5e97458e34
Laurens-Hertzer <laurens.hertzer@stud.kbw.ch>
2026-02-07 14:09:32 +0100

    possibility to send messages

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 const WebSocket = require('ws');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('WebSocket server is running');
+  if (req.url === '/' || req.url === '/index.html') {
+    const filePath = path.join(__dirname, 'index.html');
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Error loading index.html');
+        return;
+      }
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    });
+  } else {
+    res.writeHead(404);
+    res.end('Not found');
+  }
 });
 
 const wss = new WebSocket.Server({ server });
@@ -11,7 +26,11 @@ const wss = new WebSocket.Server({ server });
 wss.on('connection', (ws) => {
   console.log('Client connected');
   ws.send('Welcome to the WebSocket server!');
-  ws.on('message', (msg) => ws.send(`Server received: ${msg}`));
+
+  ws.on('message', (msg) => {
+    console.log('Received:', msg);
+    ws.send(`Server received: ${msg}`);
+  });
 });
 
 const PORT = process.env.PORT || 8080;
